78bce6096f87cb20f047c61ec9a2c1356557bc27
Changlin <CarberryChai@gmail.com>
2024-04-01 08:35:16 +0800

    Remove unused files and update header navigation

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://xiaochai-blog.pages.dev/", // replace this with your deployed domain
   author: "Changlin",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  title: "XiaoChai",
   lightAndDarkMode: true,
   postPerPage: 5,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
