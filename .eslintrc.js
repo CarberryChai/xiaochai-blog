c265380c867e1e7d100129a96da09c203ab5e349
Changlin <CarberryChai@gmail.com>
2024-04-06 14:41:19 +0800

    Update code formatting and imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,21 @@ module.exports = {
     es2022: true,
     browser: true,
   },
-  extends: ["eslint:recommended", "plugin:astro/recommended"],
-  parser: "@typescript-eslint/parser",
+  extends: ['eslint:recommended', 'plugin:astro/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   overrides: [
     {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
       },
       rules: {},
     },
   ],
-};
+}
